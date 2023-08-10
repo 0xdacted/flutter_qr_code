ce24c5f368a0ed9305de9f9667ce5b601c62c319
Red <crabmarket56@gmail.com>
2023-08-10 21:33:59 +0900

    Add QRCode class outline

---
+++ lib/qr_code.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+class QRCode extends StatelessWidget {
+  const QRCode({
+    Key? key,
+    this.width,
+    this.height,
+  }) : super(key: key);
+
+  final double? width;
+  final double? height;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Replace with the QrImage widget
+    // of the package
+    return Container();
+  }
+}
